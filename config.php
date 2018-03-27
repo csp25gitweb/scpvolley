c753356b794a841eeab7804a93c1a00faad8f0d8
csp37 <csp37@z031enfp-1315.infra.enc>
2018-03-27 09:45:26 +0200

    create file config

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+// paramétrage du serveur
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// paramétrage BDD
+$params_bdd = array(
+    'host' => '10.189.251.9',
+    'bdd' => 'scp4',
+    'login' => 'scp4',
+    'pwd' => 'scp4',
+    'port' => '5432',
+);
+
