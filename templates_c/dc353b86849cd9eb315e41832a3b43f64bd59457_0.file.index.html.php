0e44f833f1fef5effcf48b9ad624be329269e8a2
csp37 <csp37@z031enfp-1315.infra.enc>
2018-03-27 09:35:26 +0200

    reorga smarty + add bootstrap lib

---
+++ templates_c/dc353b86849cd9eb315e41832a3b43f64bd59457_0.file.index.html.php
@@ -0,0 +1,38 @@
+<?php
+/* Smarty version 3.1.30, created on 2018-03-27 09:10:07
+  from "/home/csp37/public_html/scpvolley/templates/index.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5ab9ee4fa0aa71_12636221',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'dc353b86849cd9eb315e41832a3b43f64bd59457' => 
+    array (
+      0 => '/home/csp37/public_html/scpvolley/templates/index.html',
+      1 => 1522134427,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5ab9ee4fa0aa71_12636221 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+<html>
+    <head>
+        <title>Titre</title>
+    </head>
+    <body>
+        
+        <p><?php echo $_smarty_tpl->tpl_vars['content']->value;?>
+</p>
+        
+    </body>
+</html>
+<?php }
+}
