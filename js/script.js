7e52bb593a86f734d72424325eeb01ee32f72673
csp25gitweb <steven.dumont@dgfip.finances.gouv.fr>
2018-04-12 14:07:07 +0200

    Fichier JS - test du comment que c'est qu'on merge

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
 (function( $ ) {
-	
-	function showModal(title, content, title_close_button, callback_valid = '', title_valid_button = 'Valider') {
-		$('#scp_modal_title').html(title);
-		$('#scp_modal_content').html(content);
-		$('#scp_modal_close_button').html(title_close_button);
-		
-		if(callback_valid != '') {
-			$('#scp_modal_valid_button')
-				.html(title_valid_button)
-				.removeClass('hidden')
-				.click(function() {
-					eval(callback_valid + '()');
-					
-					$(this)
-						.prop('disabled', true)
-						.html('Chargement...')
-					;
-					
-					$('#scp_modal_close_button').prop('disabled', true);
-					$('#scp_modal_close_button_small').hide();
-				})
-			;
-		}
-		
-		$('#scp_modal').modal({backdrop: 'static', keyboard: false}); 
-	}
-	
-	function testCallback() {
-		console.log('test callback');
-	}
-	
-	showModal('test', '<p><strong>test</strong> contenu</p>', 'titre bouton close', 'testCallback', 'override titre valider');
+    //Fonction modale
+    function showModal(title, content, title_close_button, callback_valid = '', title_valid_button = 'Valider') {
+            $('#scp_modal_title').html(title);
+            $('#scp_modal_content').html(content);
+            $('#scp_modal_close_button').html(title_close_button);
+
+            if(callback_valid != '') {
+                    $('#scp_modal_valid_button')
+                            .html(title_valid_button)
+                            .removeClass('hidden')
+                            .click(function() {
+                                    eval(callback_valid + '()');
+
+                                    $(this)
+                                            .prop('disabled', true)
+                                            .html('Chargement...')
+                                    ;
+
+                                    $('#scp_modal_close_button').prop('disabled', true);
+                                    $('#scp_modal_close_button_small').hide();
+                            })
+                    ;
+            }
+
+            $('#scp_modal').modal({backdrop: 'static', keyboard: false}); 
+    }
+
+    function testCallback() {
+            console.log('test callback');
+    }
+
+    showModal('test', '<p><strong>test</strong> contenu</p>', 'titre bouton close', 'testCallback', 'override titre valider');
 
 })( jQuery );
