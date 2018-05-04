880817e94c60a75ef660fd586500e367d06e3f33
Steven DUMONT <windir10@gmail.com>
2018-05-04 11:19:47 +0200

    Dev custom notify + debug listener hideModal

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 (function( $ ) {
+	var notify;
+	
     $.showModal = function(title, content, title_close_button, callback_valid = '', title_valid_button = 'Valider') {
         $('#scp_modal_title').html(title);
         $('#scp_modal_content').html(content);
@@ -29,10 +31,48 @@
 		$('#scp_modal_valid_button')
 			.addClass('hidden')
 			.removeProp('disabled')
+			.off()
 		;
 		$('#scp_modal_close_button').removeProp('disabled');
 		$('#scp_modal_close_button_small').show();
 		$('#scp_modal').modal('hide');
+	}
+	
+	$.showNotify = function(type, title) {
+		var icon;
+		switch(type) {
+			case 'success':
+				icon = 'fa-chevron-circle-down';
+			break;
+			case 'info':
+				icon = 'fa-info-circle';
+			break;
+			case 'warning':
+				icon = 'fa-exclamation-circle';
+			break;
+			case 'danger':
+				icon = 'fa-bomb';
+			break;
+		}
+		notify = $.notify({
+			icon: 'fa fa-2x ' + icon,
+			message: title
+		},{
+			type: type,
+			mouse_over: 'pause',
+			placement: {
+				from: 'bottom',
+				align: 'center'
+			},
+			animate: {
+				enter: 'animated fadeIn',
+				exit: 'animated fadeOut'
+			}
+		});
+	}
+	
+	$.hideNotify = function() {
+		notify.close();
 	}
 
 })( jQuery );
